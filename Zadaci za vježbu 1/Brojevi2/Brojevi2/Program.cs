ba244eeba8f5c41255d5b5e6569801f380fe917c
Luka Grubić <luka.grubic2000@outlook.com>
2022-03-07 15:55:21 +0100

    Z1

---
+++ Zadaci za vježbu 1/Brojevi2/Brojevi2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brojevi2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("A = ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("B = ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("C = ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            int suma=0;
+            int zastavica = 0;
+            Console.Write("Rezultat je ");
+            for(int i = a; i <= b; i++)
+            {
+                if (i % c == 0)
+                {
+                    if (zastavica==0) Console.Write(i);
+                    else Console.Write(", " + i);
+                    suma += i;
+                    zastavica++;
+                }
+            }
+            Console.WriteLine("\nSuma je "+suma);
+
+            Console.ReadLine();
+        }
+    }
+}
