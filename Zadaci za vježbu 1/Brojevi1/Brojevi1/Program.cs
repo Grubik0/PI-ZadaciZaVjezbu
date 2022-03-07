0843f44b83ad51ee6acaccaaee0d953a7046480a
Luka Grubić <luka.grubic2000@outlook.com>
2022-03-07 15:37:01 +0100

    Z1

---
+++ Zadaci za vježbu 1/Brojevi1/Brojevi1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brojevi1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite broj jedinica: ");
+            int jedinice = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Unesite broj desetica: ");
+            int desetice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Rezultat: " + ((desetice * 10) + jedinice) * 2.1);
+            Console.ReadLine();
+        }
+    }
+}
